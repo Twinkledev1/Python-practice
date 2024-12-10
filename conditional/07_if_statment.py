9ec2422b316fa89cf2849613b231b6a596fe9ebe
Twinkledev1 <twinkledevwanshi93@gmail.com>
2024-12-09 23:10:38 -0800

    fix the file

---
+++ conditional/07_if_statment.py
@@ -0,0 +1,10 @@
+#  Python Program to Check if a Number is Odd or Even
+
+num = int(input("Enter the number:  "))
+
+if num % 2 == 0:
+ 
+   print("{0} is a even number".format(num))
+
+else:
+   print("{} is a odd number ".format(num))
